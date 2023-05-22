091b3c8de33768acaa5e65c91a0e190c171b955c
Prakash, Deva <Deva.Prakash3@T-Mobile.com>
2023-05-22 21:19:53 +0530

    removed new relic

--- mstore-api/src/main/java/com/mstore/MstoreApiApplication.java
+++ mstore-api/src/main/java/com/mstore/MstoreApiApplication.java
@@ -1,7 +1,5 @@
 package com.mstore;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,13 +10,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class MstoreApiApplication extends SpringBootServletInitializer {
 
-  private static final Logger LOGGER = LogManager.getLogger(MstoreApiApplication.class);
-
   public static void main(String[] args) {
     SpringApplication.run(MstoreApiApplication.class, args);
-    LOGGER.info("Info level log message");
-    LOGGER.debug("Debug level log message");
-    LOGGER.error("Error level log message");
   }
 
   @Override
