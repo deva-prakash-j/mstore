099f3a12a172e185c76aaf1cdcdbdabe7eacc0fa
Prakash, Deva <Deva.Prakash3@T-Mobile.com>
2023-05-22 21:04:25 +0530

    header data update

--- mstore-api/src/main/java/com/mstore/repository/ProductsRepository.java
+++ mstore-api/src/main/java/com/mstore/repository/ProductsRepository.java
@@ -20,10 +20,13 @@ public interface ProductsRepository extends MongoRepository<ProductsEntity, Stri
   List<ProductsEntity> getProductsWithEmptyProductVariants();
 
   @Aggregation(pipeline = {"{ $facet: {\r\n"
-      + "      categories: [{ $group: { _id: \"categories\", categories:{$addToSet:\"$category\"} } }, { \"$project\": {_id: 0}}],\r\n"
-      + "      brands:     [{ $group: { _id: \"brands\",    brands:{$addToSet:\"$brand\"} } }, { \"$project\": {_id: 0}}],\r\n"
-      + "      brandsWithCategory:[{ $group: { _id: {category: \"$category\", brand:\"$brand\"}, \"count\": { \"$sum\": 1 }  } }, { $group: { _id: \"$_id.category\", brands:{$addToSet:\"$_id.brand\"} } }]\r\n"
-      + "  }}"})
+      + "      categories: [{ $group: { _id: \"$categorySlug\", categories:{$addToSet:\"$category\"} }},{ \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$categories\" }}}],\r\n"
+      + "      brands:     [{ $group: { _id: \"$brandSlug\", brands:{$addToSet:\"$brand\"} } }, { \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$brands\" }}}],\r\n"
+      + "      brandsWithCategory:[\r\n"
+      + "      { $group: { _id: {categorySlug: \"$categorySlug\", category: \"$category\", brand: {urlSlug: \"$brandSlug\", displayText:\"$brand\"}}, \"count\": { \"$sum\": 1 }}},\r\n"
+      + "      { $group: { _id: \"$_id.category\", categorySlugs:{$addToSet:\"$_id.categorySlug\"},brands:{$addToSet:\"$_id.brand\"}}},\r\n"
+      + "      { \"$project\": {_id: 0, \"urlSlug\":{ $first: \"$categorySlugs\" }, \"displayText\": \"$_id\", brands: \"$brands\"}}\r\n"
+      + "      ]\r\n" + "  }}"})
   public List<ProductsEntity> fetchHeaderData();
 
 }
