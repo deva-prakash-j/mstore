3b54f908286163ad318a4e451ea0a3c0ebc2243f
Prakash, Deva <Deva.Prakash3@T-Mobile.com>
2023-05-23 08:18:44 +0530

    sort query

--- mstore-api/src/main/java/com/mstore/repository/ProductsRepository.java
+++ mstore-api/src/main/java/com/mstore/repository/ProductsRepository.java
@@ -19,14 +19,13 @@ public interface ProductsRepository extends MongoRepository<ProductsEntity, Stri
   @Query("{productVariants : []}")
   List<ProductsEntity> getProductsWithEmptyProductVariants();
 
-  @Aggregation(pipeline = {"{ $facet: {\r\n"
-      + "      categories: [{ $group: { _id: \"$categorySlug\", categories:{$addToSet:\"$category\"} }},{ \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$categories\" }}}],\r\n"
-      + "      brands:     [{ $group: { _id: \"$brandSlug\", brands:{$addToSet:\"$brand\"} } }, { \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$brands\" }}}],\r\n"
+  @Aggregation(pipeline = {"{$facet:{\r\n"
+      + "      categories: [{ $group: { _id: \"$categorySlug\", categories:{$addToSet:\"$category\"} }},{ $sort : { _id : 1 } }, { \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$categories\" }}}],\r\n"
+      + "      brands: [{ $group: { _id: \"$brandSlug\", brands:{$addToSet:\"$brand\"} } },{ $sort : { _id : 1 } }, { \"$project\": {_id: 0, \"urlSlug\": \"$_id\", \"displayText\": { $first: \"$brands\" }}}],\r\n"
       + "      brandsWithCategory:[\r\n"
       + "      { $group: { _id: {categorySlug: \"$categorySlug\", category: \"$category\", brand: {urlSlug: \"$brandSlug\", displayText:\"$brand\"}}, \"count\": { \"$sum\": 1 }}},\r\n"
-      + "      { $group: { _id: \"$_id.category\", categorySlugs:{$addToSet:\"$_id.categorySlug\"},brands:{$addToSet:\"$_id.brand\"}}},\r\n"
-      + "      { \"$project\": {_id: 0, \"urlSlug\":{ $first: \"$categorySlugs\" }, \"displayText\": \"$_id\", brands: \"$brands\"}}\r\n"
-      + "      ]\r\n" + "  }}"})
+      + "      { $group: { _id: \"$_id.category\", categorySlugs:{$addToSet:\"$_id.categorySlug\"},brands:{$addToSet:\"$_id.brand\"}}},{ $sort : { _id : 1 } },\r\n"
+      + "      { \"$project\": {_id: 0, \"urlSlug\":{ $first: \"$categorySlugs\" }, \"displayText\": \"$_id\", brands: {$sortArray: { input: \"$brands\", sortBy: { displayText: 1 } }}}}]}}"})
   public List<ProductsEntity> fetchHeaderData();
 
 }
